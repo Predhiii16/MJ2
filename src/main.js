009d0bb13af71466e1fb8c35e0b312cbb56ad015
pradhii <pradhiii28@gmail.com>
2026-09-04 15:41:43 +0530

    Fix ScrollTrigger import

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { memoryData } from './data/memories.js';
 import gsap from 'https://esm.sh/gsap';
-import { ScrollTrigger } from 'https://esm.sh/gsap/ScrollTrigger';
+import ScrollTrigger from 'https://esm.sh/gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
